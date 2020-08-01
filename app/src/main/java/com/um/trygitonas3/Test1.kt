24ca79d662fffb1b2d10bfa1c4356f6a059d0f9a
Eric <renovaeric@gmail.com>
2020-08-01 09:52:55 +0800

    add 3rd line of comment with add Test2.kt file.

--- app/src/main/java/com/um/trygitonas3/Test1.kt
+++ app/src/main/java/com/um/trygitonas3/Test1.kt
@@ -2,6 +2,7 @@ package com.um.trygitonas3
 
 // this is a test for git
 // add 2nd line of comment
+// add 3rd line of comment
 
 class Test1 {
 }
---
+++ app/src/main/java/com/um/trygitonas3/Test2.kt
@@ -0,0 +1,4 @@
+package com.um.trygitonas3
+
+class Test2 {
+}
