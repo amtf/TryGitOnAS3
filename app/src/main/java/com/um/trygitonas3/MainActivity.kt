e034a2acfaed05b3784b57cb4bfb6fbe5996a7d4
Eric <renovaeric@gmail.com>
2020-08-01 10:31:02 +0800

    Add the whole project to github

---
+++ app/src/main/java/com/um/trygitonas3/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.um.trygitonas3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// add line for testing the git
+// add one more line
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
